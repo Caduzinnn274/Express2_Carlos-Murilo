20e95f6e93ef2e3183c9b8470d940c5531242916
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-08-05 10:23:46 -0300

    Update server2.js

--- server2.js
+++ server2.js
@@ -6,8 +6,9 @@ server2.listen(3000);
 
 
 // server2.use(morgan("tiny"))
+// server2.use(morgan(":method :url :status"))
 
-server2.use(morgan(":method :url :status"))
+server2.use(morgan("Método = :method | Status = :status | Url = :url"))
 
 server2.get("/", (req,res)=>{
     res.send("Teste de conexão de server2")
