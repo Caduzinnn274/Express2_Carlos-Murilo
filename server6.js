50d565d22c46b640713f5bca82c49baf53ea544b
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-08-09 11:25:08 -0300

    server6

---
+++ server6.js
@@ -0,0 +1,15 @@
+//incorporar  respostas a partir 
+
+const express = require("express");
+const app = express();
+
+app.listen(3000,()=>{
+    console.log("Servidor no ar mano!!!");
+
+})
+
+//criar arquivo functions
+
+
+//importar as funções
+const funcoes = require('.')
