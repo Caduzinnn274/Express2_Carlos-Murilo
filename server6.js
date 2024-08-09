2a692f3a4512adf4bf7fbe0c0b8bc9606008c83e
Carlos Bressan <105678823+Caduzinnn274@users.noreply.github.com>
2024-08-09 12:06:58 -0300

    Update server6.js

--- server6.js
+++ server6.js
@@ -28,9 +28,24 @@ app.get('/add/:a/:b',(req,res)=>{
 
 //criar rota com hífen
 
-app.get('/add/:a-:b',(req,res)=>{
+app.get('/sub/:a-:b',(req,res)=>{
     const a = parseInt(req.params.a);
     const b = parseInt(req.params.b);
-    const resultado = funcoes.add(a,b);
-    res.send(`A soma de ${a} com ${b} é ${resultado}`);
+    const resultado = funcoes.sub(a,b);
+    res.send(`A subtração de ${a} com ${b} é ${resultado}`);
+})
+
+
+app.get('/mult/:a-:b',(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+    const resultado = funcoes.mult(a,b);
+    res.send(`A multiplicação de ${a} com ${b} é ${resultado}`);
+})
+
+app.get('/div/:a-:b',(req,res)=>{
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+    const resultado = funcoes.div(a,b);
+    res.send(`A divisão de ${a} com ${b} é ${resultado}`);
 })
